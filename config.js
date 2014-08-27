86145ebf01e98e1464251b40660ecc1636f84873
Benjamin De Cock <bdc@stripe.com>
2014-08-27 13:46:37 +0200

    App

---
+++ config.js
@@ -0,0 +1,10 @@
+// Howdy! This is your website's configuration file.
+// Just replace my personal info with yours, save, and you're done :)
+
+dribbbox.config = {
+
+  dribbble_username : "bdc",
+  short_description : "User interface and icon designer.",
+  email_address     : "ben@deaxon.com"
+
+}
